a1a21a92aead887e4af352cf515107fdd70bd85e
wsameer <dev.wsameer@gmail.com>
2025-10-31 10:51:38 -0300

    pilot commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/hello", (c) => {
+  return c.json({
+    ok: true,
+    message: "Hello Hono!",
+  });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
