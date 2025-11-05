7c3dc4083a39765744967e8b0f626c12348e43e1
wsameer <dev.wsameer@gmail.com>
2025-11-04 20:31:00 -0500

    updates to config/env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,13 @@
 export const env = {
   PORT: parseInt(process.env.PORT || "3000"),
-  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+
   BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
   BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+
+  DATABASE_URL: process.env.DATABASE_URL,
+
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
 };
